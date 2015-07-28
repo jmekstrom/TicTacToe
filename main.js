7b436e76da1f13ed065da3389e4607868a8964db
Andrew Lopez <Andy@Andrews-MacBook-Pro.local>
2015-07-27 17:51:53 -0700

    html gets built by jquermo

---
+++ main.js
@@ -0,0 +1,22 @@
+function addWaffle() {
+$('#gameboard').html(
+'<div class="container">'+
+    '<div class="smWaffleGrid">'+
+        '<div class="row">'+
+        	'<div class="slot"></div>'+
+        	'<div class="slot"></div>'+
+        	'<div class="slot"></div>'+
+        '<div>'+
+        '<div class="row">'+
+        	'<div class="slot"></div>'+
+        	'<div class="slot"></div>'+
+        	'<div class="slot"></div>'+
+        '<div>'+
+        '<div class="row">'+
+        	'<div class="slot"></div>'+
+        	'<div class="slot"></div>'+
+        	'<div class="slot"></div>'+
+        '<div>'+
+    '</div>'+
+'</div>');
+}
