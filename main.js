c20fee3fa0576f161f4dfb9d550bcf56008a41d2
Jason Ekstrom <JEkstroms@Jasons-MacBook-Pro.local>
2015-07-28 16:37:59 -0700

    win array generation function added

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ function smWaffle() {
         '<div class="container">' +
         '<div class="smWaffleGrid">' +
         '<div class="row">' +
-        '<div class="slot" onclick=XO(1)></div>' +
-        '<div class="slot" onclick=XO(2)></div>' +
-        '<div class="slot" onclick=XO(3)></div>' +
+        '<div class="slot" id="slot1" onclick=XO(1)></div>' +
+        '<div class="slot" id="slot2" onclick=XO(2)></div>' +
+        '<div class="slot" id="slot3" onclick=XO(3)></div>' +
         '</div>' +
         '<div class="row">' +
-        '<div class="slot" onclick=XO(4)></div>' +
-        '<div class="slot" onclick=XO(5)></div>' +
-        '<div class="slot" onclick=XO(6)></div>' +
+        '<div class="slot" id="slot4" onclick=XO(4)></div>' +
+        '<div class="slot" id="slot5" onclick=XO(5)></div>' +
+        '<div class="slot" id="slot6" onclick=XO(6)></div>' +
         '<div>' +
         '</div class="row">' +
-        '<div class="slot" onclick=XO(7)></div>' +
-        '<div class="slot" onclick=XO(8)></div>' +
-        '<div class="slot" onclick=XO(9)></div>' +
+        '<div class="slot" id="slot7" onclick=XO(7)></div>' +
+        '<div class="slot" id="slot8" onclick=XO(8)></div>' +
+        '<div class="slot" id="slot9" onclick=XO(9)></div>' +
         '</div>' +
         '</div>' +
         '</div>');
@@ -179,4 +179,57 @@ function lgWaffle() {
         '</div>' +
         '</div>' +
         '</div>');
+}
+
+function XO(n) {
+    console.log("Slot:", n, "has been clicked");
+    Build_Win_Array(n);
+}
+
+function Build_Win_Array(width){
+
+    var size = width;
+    var Win_Condition = [];
+    var Win_Array = [];
+
+    //horizontal conditions
+    for (var i = 0; i < size * size; i += size) {
+        for (var j = i; j < size + i; j++) {
+            Win_Condition.push(j);
+        }
+        console.log("Win Condition", Win_Condition);
+        Win_Array.push(Win_Condition);
+        Win_Condition = [];
+    }
+    //console.log("WA", WA);
+
+    //vertical conditions
+    for (var i = 0; i < size; i++) {
+        for (var j = i; j < size * size; j += size) {
+            Win_Condition.push(j);
+        }
+        console.log("Win Condition", Win_Condition);
+        Win_Array.push(Win_Condition);
+        Win_Condition = [];
+    }
+    //console.log("WA", WA);
+
+    //diagonal tl to br
+    for (var i = 0; i < size * size; i += size + 1) {
+        Win_Condition.push(i);
+    }
+    console.log("Win Condition", Win_Condition);
+    Win_Array.push(Win_Condition);
+    //console.log("WA", WA);
+    Win_Condition = [];
+
+    //diagonal tr to bl
+    for (var i = size - 1; i <= size * size - size; i += size - 1) {
+        Win_Condition.push(i);
+    }
+    console.log("Win Condition", Win_Condition);
+    Win_Array.push(Win_Condition);
+    console.log("Win Array", Win_Array);
+    Win_Condition = [];
+
 }
