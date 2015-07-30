4f10c3e9ebaab0f79bf9009fe4e6f3ba1be63e1e
Jason Ekstrom <JEkstroms@Jasons-MacBook-Pro.local>
2015-07-29 18:18:10 -0700

    added styling for now

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var player = 1;
-var player_clicks = [0,1,2,3,4,5,6,7,8];
+var player_clicks = [1,2,3,4,5,6,7,8,9];
 var Win_Array = [];
 var board_width;
+var click_num = 0;
 
 function smWaffle() {
     $('#gameboard').html(
@@ -16,8 +17,8 @@ function smWaffle() {
         '<div class="slot" id="slot4" onclick=XO(4)></div>' +
         '<div class="slot" id="slot5" onclick=XO(5)></div>' +
         '<div class="slot" id="slot6" onclick=XO(6)></div>' +
-        '<div>' +
-        '</div class="row">' +
+        '</div>' +
+        '<div class="row">' +
         '<div class="slot" id="slot7" onclick=XO(7)></div>' +
         '<div class="slot" id="slot8" onclick=XO(8)></div>' +
         '<div class="slot" id="slot9" onclick=XO(9)></div>' +
@@ -187,19 +188,26 @@ function lgWaffle() {
 }
 
 function XO(n) {
+    click_num++;
     console.log("Slot:", n, "has been clicked");
     $("#slot" + n).removeAttr('onclick');
     if (player == 1) {
         player_clicks[n-1] = "x";
         player = 2;
-        $("#slot" + n).text("X");
+        $("#slot" + n).append("<img class='tile_img' src='images/blueberry.png'>");
     } else {
         player_clicks[n-1] = "o";
         player = 1;
-        $("#slot" + n).text("O");
+        $("#slot" + n).append("<img class='tile_img' src='images/raspberry.png'>");
     }
     console.log(player_clicks)
-    win_check();
+    var won = win_check();
+    if(won != undefined){
+        console.log("Game Won by",won)
+    }
+    if(click_num==9 && won == undefined){
+        console.log("Tie Game")
+    }
 }
 
 function win_check() {
@@ -207,20 +215,22 @@ function win_check() {
 
     //Outer for loop iterates the each individual win condition
     for (var i = 0; i < Win_Array.length; i++) {
-        var match = 0;
         //Inner for loop interates through the win condition array (nested in the win array) 
         //and points to the player click array and looks for the amount of matches equal to 
         //board width -1
+        var match = 0;
+        var previous_value = null;
         for (var j = 0; j < Win_Array[i].length; j++) {
             var current_value = player_clicks[Win_Array[i][j]];
+            //console.log("cv:",current_value,", pv:",previous_value)
             if (current_value == previous_value) {
                 match++;
+                console.log("match:",match)
                 if (match == board_width - 1) {
-                    console.log("game won by:",current_value);
+                    return current_value
                 }
             }
             var previous_value = player_clicks[Win_Array[i][j]];
-
         }
     }
 }
